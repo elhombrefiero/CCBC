9eb4183d76be8bf88e16ae8654a580744ebf5e2d
Rene Valdez <elhombrefiero@outlook.com>
2018-01-08 21:16:04 -0500

    Generalized function in the simple arduino code.

--- Arduino/Arduino_Control_Simple/Arduino_Control_Simple.ino
+++ Arduino/Arduino_Control_Simple/Arduino_Control_Simple.ino
@@ -1,30 +1,17 @@
 // Listens to inputs coming from the serial to
 // simply open and shut switches (pins)
 
-// Configured to 8 pins, defined below
 
-int pin1 = 2;
-int pin2 = 3;
-int pin3 = 4;
-int pin4 = 5;
-int pin5 = 6;
-int pin6 = 7;
-int pin7 = 8;
-int pin8 = 9;
+
 String readString;
 String pinStatus;
 
 void setup()
 {
-    // All pins are defined as OUTPUT
-  pinMode(pin1, OUTPUT); 
-  pinMode(pin2, OUTPUT);
-  pinMode(pin3, OUTPUT);
-  pinMode(pin4, OUTPUT);
-  pinMode(pin5, OUTPUT);
-  pinMode(pin6, OUTPUT);
-  pinMode(pin7, OUTPUT);
-  pinMode(pin8, OUTPUT);
+  // Set pins 2-13 as output
+  for (int i=2; i<14; i++) {
+    pinMode(i, OUTPUT);
+  }
   Serial.begin(9600);
   while (! Serial); // Wait untilSerial is ready - Leonardo
   Serial.print("Testing input to serial");
