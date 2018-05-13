51b0840c08a9404c98129e6d994dfe7b36b7895c
Rene Valdez <elhombrefiero@outlook.com>
2018-05-13 12:24:26 -0400

    Checking to see if this method of threading works.

--- Python/ccbc_gui.py
+++ Python/ccbc_gui.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env Python3
 import sys
 import time
-from PyQt5.QtCore import QThread, QTimer
+from PyQt5.QtCore import QThread, QTimer, pyqtSignal
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from theGUI import Ui_MainWindow
 
@@ -23,61 +23,22 @@ class MyThread(QThread):
     def update_and_execute(self):
         self.ccbc.updateAndExecute()
 
-class LabelThread(QThread):
+class VariablesThread(QThread):
     """ Thread class used to update the dynamic labels in the GUI"""
+    temp1Signal = pyqtSignal(float)
+    temp2Signal = pyqtSignal(float)
 
     def __init__(self, ccbc, parent=None):
-        super(LabelThread, self).__init__(parent)
+        super(Variables, self).__init__(parent)
         self.ccbc = ccbc
         self.timer = QTimer()
 
     def run(self):
-        self.timer.timeout.connect(self.update_labels)
-        self.timer.start(250)
+        while True:
+            self.temp1Signal.emit(self.ccbc.t_sensors[0].cur_temp)
+            self.sleep(1)
 
-    def update_labels(self):
 
-        # Status Page
-        self.VariableT1.setText(str(self.ccbc.t_sensors[0].cur_temp))
-        self.VariableT2.setText(str(self.ccbc.t_sensors[1].cur_temp))
-        self.VariableT3.setText(str(self.ccbc.t_sensors[2].cur_temp))
-        self.VariableT4.setText(str(self.ccbc.t_sensors[3].cur_temp))
-        self.VariableT5.setText(str(self.ccbc.t_sensors[4].cur_temp))
-        self.VariableT6.setText(str(self.ccbc.t_sensors[5].cur_temp))
-        self.VariableT7.setText(str(self.ccbc.t_sensors[6].cur_temp))
-        self.VariableT8.setText(str(self.ccbc.t_sensors[7].cur_temp))
-        self.VariableT9.setText(str(self.ccbc.t_sensors[8].cur_temp))
-
-        self.VariablePress1.setText(str(self.ccbc.p_sensors[0].current_pressure))
-        self.VariablePress2.setText(str(self.ccbc.p_sensors[1].current_pressure))
-        self.VariablePress3.setText(str(self.ccbc.p_sensors[2].current_pressure))
-        self.VariablePress4.setText(str(self.ccbc.p_sensors[3].current_pressure))
-
-        self.VariableH1.setText(self.ccbc.heaters[0].returnPinStatus())
-        self.VariableH2.setText(self.ccbc.heaters[1].returnPinStatus())
-        self.VariableH3.setText(self.ccbc.heaters[2].returnPinStatus())
-
-        self.VariablePump1.setText(self.ccbc.pumps[0].returnPinStatus())
-        self.VariablePump2.setText(self.ccbc.pumps[1].returnPinStatus())
-        self.VariablePump3.setText(self.ccbc.pumps[2].returnPinStatus())
-
-        # Heater 1 Page
-        self.VariableHeater1Temp.setText(self.ccbc.heaters[0].returnCurrentTemp())
-        self.VariableHeater1Status.setText(self.ccbc.heaters[0].returnPinStatus())
-        self.VariableHeater1Setpoint.setText(str(self.ccbc.heaters[0].temperature_setpoint))
-        self.VariableHeater1MaxTemp.setText(str(self.ccbc.heaters[0].max_temp))
-
-        # Heater 2 Page
-        self.VariableHeater2Temp.setText(self.ccbc.heaters[1].returnCurrentTemp())
-        self.VariableHeater2Status.setText(self.ccbc.heaters[1].returnPinStatus())
-        self.VariableHeater2Setpoint.setText(str(self.ccbc.heaters[1].temperature_setpoint))
-        self.VariableHeater2MaxTemp.setText(str(self.ccbc.heaters[1].max_temp))
-
-        # Heater 3 Page
-        self.VariableHeater3Temp.setText(self.ccbc.heaters[2].returnCurrentTemp())
-        self.VariableHeater3Status.setText(self.ccbc.heaters[2].returnPinStatus())
-        self.VariableHeater3Setpoint.setText(str(self.ccbc.heaters[2].temperature_setpoint))
-        self.VariableHeater3MaxTemp.setText(str(self.ccbc.heaters[2].max_temp))
 
 class ccbcGUI(QMainWindow, Ui_MainWindow):
 
@@ -86,7 +47,8 @@ class ccbcGUI(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.ccbc = ccbc
         self.SerThread = MyThread(self.ccbc)
-        self.LabelThread = LabelThread(self.ccbc)
+        self.VariablesThread = VariablesThread(self.ccbc)
+        self.VariablesThread.temp1Signal.connect(self.updateTemp1)
         self.Button_startSerial.clicked.connect(self.start_everything)
         self.ButtonUpdateHeater1Setpoint.clicked.connect(self.update_heater1_setpoint)
         self.ButtonUpdateHeater2Setpoint.clicked.connect(self.update_heater2_setpoint)
@@ -96,6 +58,9 @@ class ccbcGUI(QMainWindow, Ui_MainWindow):
         self.ButtonUpdateHeater3MaxTemp.clicked.connect(self.update_heater3_maxtemp)
         self.update_static_labels()
         self.show()
+
+    def updateTemp1(self, temp):
+        sself.VariableT1.setText(str(temp))
 
     def update_heater1_setpoint(self):
         setpoint = self.InputHeater1Setpoint.toPlainText()
@@ -172,6 +137,50 @@ class ccbcGUI(QMainWindow, Ui_MainWindow):
         self.LabelPump1.setText(self.ccbc.pumps[0].name)
         self.LabelPump2.setText(self.ccbc.pumps[1].name)
         self.LabelPump3.setText(self.ccbc.pumps[2].name)
+
+        self.VariablePump1.setText(self.ccbc.pumps[0].returnPinStatus())
+        self.VariablePump2.setText(self.ccbc.pumps[1].returnPinStatus())
+        self.VariablePump3.setText(self.ccbc.pumps[2].returnPinStatus())
+
+        # Heater 1 Page
+        self.VariableHeater1Temp.setText(self.ccbc.heaters[0].returnCurrentTemp())
+        self.VariableHeater1Status.setText(self.ccbc.heaters[0].returnPinStatus())
+        self.VariableHeater1Setpoint.setText(str(self.ccbc.heaters[0].temperature_setpoint))
+        self.VariableHeater1MaxTemp.setText(str(self.ccbc.heaters[0].max_temp))
+
+        # Heater 2 Page
+        self.VariableHeater2Temp.setText(self.ccbc.heaters[1].returnCurrentTemp())
+        self.VariableHeater2Status.setText(self.ccbc.heaters[1].returnPinStatus())
+        self.VariableHeater2Setpoint.setText(str(self.ccbc.heaters[1].temperature_setpoint))
+        self.VariableHeater2MaxTemp.setText(str(self.ccbc.heaters[1].max_temp))
+
+        # Heater 3 Page
+        self.VariableHeater3Temp.setText(self.ccbc.heaters[2].returnCurrentTemp())
+        self.VariableHeater3Status.setText(self.ccbc.heaters[2].returnPinStatus())
+        self.VariableHeater3Setpoint.setText(str(self.ccbc.heaters[2].temperature_setpoint))
+        self.VariableHeater3MaxTemp.setText(str(self.ccbc.heaters[2].max_temp))
+
+    def update_labels(self):
+
+        # Status Page
+        self.VariableT1.setText(str(self.ccbc.t_sensors[0].cur_temp))
+        self.VariableT2.setText(str(self.ccbc.t_sensors[1].cur_temp))
+        self.VariableT3.setText(str(self.ccbc.t_sensors[2].cur_temp))
+        self.VariableT4.setText(str(self.ccbc.t_sensors[3].cur_temp))
+        self.VariableT5.setText(str(self.ccbc.t_sensors[4].cur_temp))
+        self.VariableT6.setText(str(self.ccbc.t_sensors[5].cur_temp))
+        self.VariableT7.setText(str(self.ccbc.t_sensors[6].cur_temp))
+        self.VariableT8.setText(str(self.ccbc.t_sensors[7].cur_temp))
+        self.VariableT9.setText(str(self.ccbc.t_sensors[8].cur_temp))
+
+        self.VariablePress1.setText(str(self.ccbc.p_sensors[0].current_pressure))
+        self.VariablePress2.setText(str(self.ccbc.p_sensors[1].current_pressure))
+        self.VariablePress3.setText(str(self.ccbc.p_sensors[2].current_pressure))
+        self.VariablePress4.setText(str(self.ccbc.p_sensors[3].current_pressure))
+
+        self.VariableH1.setText(self.ccbc.heaters[0].returnPinStatus())
+        self.VariableH2.setText(self.ccbc.heaters[1].returnPinStatus())
+        self.VariableH3.setText(self.ccbc.heaters[2].returnPinStatus())
 
         self.VariablePump1.setText(self.ccbc.pumps[0].returnPinStatus())
         self.VariablePump2.setText(self.ccbc.pumps[1].returnPinStatus())
